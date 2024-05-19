39707a57dda3ad51ea8c01c514845045aa5457ee
Siddharth27Roy <siddharth27.roy27@gmail.com>
2024-05-20 00:30:13 +0530

    login and verify page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,72 @@
-import React from 'react'
+'use client'
 
-function page() {
-  return (
-    <div>
-      login page
-    </div>
-  )
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import  Link  from 'next/link'; 
+
+export default function LoginPage() {
+
+	const router = useRouter()
+
+	const [user, setUser] = useState({
+		email: "",
+		password: "",
+	});
+
+	const [buttonDisabled, setButtonDisabled] = useState(false);
+	const [loading, setLoading] =useState(false);
+
+	const onLogin = async () => {
+		try {
+			
+			setLoading(true);
+			const response = await axios.post("/api/users/login", user);
+			console.log("Login success", response.data);
+			router.push('/profile');
+
+		} catch (error:any) {
+			console.log("Login failed");
+			toast.error(error.message)
+		}
+	}
+
+	useEffect(() => {
+		if(user.email.length > 0 && user.password.length > 0){
+			setButtonDisabled(false);
+		}else{
+			setButtonDisabled(true);
+		}
+	}, [user]);
+
+	return (
+		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
+			<h1>{loading? "Processing" : "Login"}</h1>
+			<hr />
+
+			<input 
+			className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+			id='email'
+			value={user.email}
+			onChange={(e) => setUser({...user, email: e.target.value})}
+			placeholder='Email'
+			type="text" />
+
+			<input 
+			className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+			id='password'
+			value={user.password}
+			onChange={(e) => setUser({...user, password: e.target.value})}
+			placeholder='Password'
+			type="password" />
+
+			<button onClick={onLogin} className='p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600'>
+				{buttonDisabled? "Enter the details" : "Login"}
+			</button>
+
+			<p>Don't have an account?  <Link href={'/signup'}>Click here to signup</Link></p>
+
+		</div>
+	)
 }
-
-export default page
